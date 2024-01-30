f6eb99b1823429e0fa2866b542539137a77141d2
Dingning Li <156145219+iamldn2002@users.noreply.github.com>
2024-01-30 10:49:37 -0500

    Delete script directory

--- script/01_downloaddate.R
+++
@@ -1,9 +0,0 @@
-raw_data <-
-  list_package_resources("neighbourhood-crime-rates") |>
-  filter(name == "neighbourhood-crime-rates - 4326.csv") |>
-  get_resource()
-
-write_csv(
-  x = raw_data,
-  file = "raw_data.csv"
-)
